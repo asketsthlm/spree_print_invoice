dd73893db6daf3a2aa79bc0e09432d815ebd6356
Mehdi Rejraji <mehdi@houseofradon.com>
2015-07-06 16:10:36 +0200

    Removed font def from the core package

--- lib/prawn_handler.rb
+++ lib/prawn_handler.rb
@@ -13,37 +13,6 @@ module ActionView
           def call(template)
             <<-PDF
               extend #{DocumentProxy}
-              pdf.font_families.update(
-                "ChronicleDisp" => {
-                  :normal => "/var/www/asket-backend/app/assets/fonts/ChronicleDisp-Light.ttf",
-                  :bold => "/var/www/asket-backend/app/assets/fonts/ChronicleDisp-Light.ttf",
-                  :bold_italic => "/var/www/asket-backend/app/assets/fonts/ChronicleDisp-Light.ttf",
-                  :italic => "/var/www/asket-backend/app/assets/fonts/ChronicleDisp-Light.ttf"
-                },
-                "FoundersGrotesk" => {
-                  :normal => "/var/www/asket-backend/app/assets/fonts/FoundersGrotesk-Light.ttf",
-                  :bold => "/var/www/asket-backend/app/assets/fonts/FoundersGrotesk-Medium.ttf",
-                  :bold_italic => "/var/www/asket-backend/app/assets/fonts/FoundersGrotesk-Medium.ttf",
-                  :italic => "/var/www/asket-backend/app/assets/fonts/FoundersGrotesk-Light.ttf"
-                },
-                "chinese" => {
-                  :normal => "/var/www/asket-backend/app/assets/fonts/chinese.ttf",
-                  :bold => "/var/www/asket-backend/app/assets/fonts/chinese.ttf",
-                  :bold_italic => "/var/www/asket-backend/app/assets/fonts/chinese.ttf",
-                  :italic => "/var/www/asket-backend/app/assets/fonts/chinese.ttf"
-                },
-                "japanese" => {
-                  :normal => "/var/www/asket-backend/app/assets/fonts/japanese.ttf",
-                  :bold => "/var/www/asket-backend/app/assets/fonts/japanese.ttf",
-                  :bold_italic => "/var/www/asket-backend/app/assets/fonts/japanese.ttf",
-                  :italic => "/var/www/asket-backend/app/assets/fonts/japanese.ttf"
-                },
-                "OpenSans" => {
-                  :normal => "/var/www/asket-backend/app/assets/fonts/OpenSans-Light.ttf",
-                  :bold => "/var/www/asket-backend/app/assets/fonts/OpenSans-Semibold.ttf",
-                  :bold_italic => "/var/www/asket-backend/app/assets/fonts/OpenSans-Semibold.ttf",
-                  :italic => "/var/www/asket-backend/app/assets/fonts/OpenSans-Light.ttf"
-                })
               #{template.source}
               pdf.render
             PDF
